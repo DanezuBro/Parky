6a651378bc86660b237b23182225566a52bee4bf
scortarudaniel <scortarudaniel@yahoo.com>
2022-06-17 17:43:30 +0300

    Adding National Parks controller and index

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _npRepo;
+
+        public NationalParksController(INationalParkRepository npRepo)
+        {
+            _npRepo = npRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });
+        }
+
+
+        #region APICalls
+
+        public async Task<IActionResult> GetAllNationalParks() 
+        {
+            return Json(new { data  = await _npRepo.GetAllAsync(SD.APIBaseUrl + SD.NationalParkAPIPath)});
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ ParkyWeb/wwwroot/js/nationalPark.js
@@ -0,0 +1,35 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable(url) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/NationalParks/GetAllNationalParks",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "state", "width": "25%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/NationalParks/Upsert/${data}" class="btn btn-primary text-white" style="cursor:pointer">
+                                    <i class="fa-solid fa-pen-to-square"></i> 
+                                </a>
+                                &nbsp;
+                                <a href="/NationalParks/Delete/${data}" class="btn btn-primary text-danger" style="cursor:pointer">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </a>
+                            </div>
+                           `;
+                }, "width": "25%"
+            }
+        ]
+    });
+}
