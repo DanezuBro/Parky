c6d5be6752cf95ea355b6d73b2728139c1ee2096
scortarudaniel <scortarudaniel@yahoo.com>
2022-06-17 15:38:43 +0300

    Add ParkyWeb MVC project, Repositories with async methods for NationalPark and Trail, IRepository and models

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:7193/";
+        public static string NationalParkAPIPath = "api/v1/nationalparks";
+        public static string TrailAPIPath = "api/v1/trails";
+    }
+}
