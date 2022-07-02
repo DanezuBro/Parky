670bf1f98808671712de2846cc69953acfbd7078
scortarudaniel <scortarudaniel@yahoo.com>
2022-07-02 16:04:55 +0300

    Removing some code.

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -99,10 +99,10 @@ namespace ParkyWeb.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
